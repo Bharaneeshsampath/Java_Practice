ebc3d193e391459066ad0f1a1d0323ee5c3ba5f8
Bharaneesh S <bharaneeshsampath28@gmail.com>
2023-12-04 13:14:31 +0530

    Commit

--- src/Works/Check_if_the_Sentence_Is_Pangram_1832.java
+++ src/Works/Check_if_the_Sentence_Is_Pangram_1832.java
@@ -1,4 +1,19 @@
 package Works;
 
+import java.util.HashSet;
+
 public class Check_if_the_Sentence_Is_Pangram_1832 {
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        HashSet<Character> alpha = new HashSet<Character>();
+        for (int i=0;i<sentence.length();i++){
+            alpha.add(sentence.charAt(i));
+        }
+        if(alpha.size()==26){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
 }
